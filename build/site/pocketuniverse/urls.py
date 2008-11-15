3f0e81e63e24e4950e212331da8b15f8be56c4ff
Sam Bull <sbull@trapeze.com>
2008-11-15 01:03:29 -0500

    Set up placeholder home page template. Added additional source image slices to repo. Copied starting css files and base template file into place. Restructured base template and home template based on design.

--- build/site/pocketuniverse/urls.py
+++ build/site/pocketuniverse/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # (r'^bonefish/', include('bonefish.foo.urls')),
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
