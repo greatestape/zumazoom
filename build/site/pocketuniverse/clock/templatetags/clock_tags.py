519f9be2977cbdc6697ba0b1400d42e59ca69e76
Sam Bull <sbull@trapeze.com>
2008-12-11 00:50:03 -0500

    Added docstring for clock_url template filter. Switched from positional to named args when calling rendering function. This is purely for clarity.

--- build/site/pocketuniverse/clock/templatetags/clock_tags.py
+++ build/site/pocketuniverse/clock/templatetags/clock_tags.py
@@ -9,6 +9,15 @@ register = template.Library()
 
 @register.filter
 def clock_url(timestamp, width):
+    """
+    Takes a datetime object and a width and returns a URL to an image
+    that contains an analog clock representation of the datetime's time,
+    plus an icon of the sun indicating time of day or night.
+    
+    Usage:
+    
+        {{ blogpost.pub_date|clock_url:60 }}
+    """
     rel_image_dir = 'managed/clock/%s/%s/' % (
         timestamp.strftime('%Y-%B').lower(),
         timestamp.strftime('%d'),
@@ -20,7 +29,7 @@ def clock_url(timestamp, width):
     image_dir_on_disk = '%s%s' % (settings.MEDIA_ROOT, rel_image_dir)
     image_path_on_disk = '%s%s' % (image_dir_on_disk, image_filename)
     if not os.path.exists(image_path_on_disk):
-        image = create_clock(timestamp, int(width) / 2, 7)
+        image = create_clock(timestamp, radius=int(width) / 2, thickness=3)
         if not os.path.exists(image_dir_on_disk):
             os.makedirs(image_dir_on_disk)
         image.save(image_path_on_disk)
